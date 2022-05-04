525f67f5b8c0cd09b0d9ad93aa0bfaf8d8ef6d31
connoredward <cchaplain94@hotmail.com>
2022-05-04 11:27:20 +0900

    _document, favicon, head title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { NextPage } from 'next';
 import useSWR from 'swr';
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
 import Nav from 'components/modules/Nav';
 import Controls from 'components/modules/Controls';
@@ -17,15 +18,20 @@ const Page: NextPage = () => {
   // Get accessToken and store as cookie.
   useSWR('/api/spotify/token');
   return (
-    <div className={styles['page']}>
-      <Nav />
-      <SearchPage />
-      <AlbumPage />
-      <QueuePage />
+    <>
+      <Head>
+        <title>msuci_pl.</title>
+      </Head>
+      <div className={styles['page']}>
+        <Nav />
+        <SearchPage />
+        <AlbumPage />
+        <QueuePage />
 
-      <Player />
-      <Controls />
-    </div>
+        <Player />
+        <Controls />
+      </div>
+    </>
   );
 };
 export default Page;
